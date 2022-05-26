8265fee5e924e1c162ebcb5905be16d546a356cb
field <jsw_777@naver.com>
2022-05-26 12:00:53 +0900

    Initial Project - jUnit 개발환경 profile 구축

---
+++ src/test/java/site/orangefield/blogv4junit/BlogV4JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.blogv4junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV4JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
