b5d82a69c4fb74faa2bf44d987626d18ec3b7d70
field <jsw_777@naver.com>
2022-05-27 09:47:36 +0900

    도메인 설계 끝

---
+++ src/main/java/site/orangefield/blogv4junit/domain/book/BookRepository.java
@@ -0,0 +1,7 @@
+package site.orangefield.blogv4junit.domain.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
