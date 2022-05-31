791f7d01cabd30b9595c46f85d64a01b691e7c64
field <jsw_777@naver.com>
2022-05-31 09:27:24 +0900

    Controller 코드 작성 완료

--- src/main/java/site/orangefield/blogv4junit/web/BookApiController.java
+++ src/main/java/site/orangefield/blogv4junit/web/BookApiController.java
@@ -2,17 +2,34 @@ package site.orangefield.blogv4junit.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import site.orangefield.blogv4junit.service.BookService;
+import site.orangefield.blogv4junit.web.dto.BookRespDto;
 import site.orangefield.blogv4junit.web.dto.BookSaveReqDto;
 
+@RequiredArgsConstructor
 @RestController
 public class BookApiController {
 
+    private final BookService bookService;
+
     @PostMapping("/api/book")
     public ResponseEntity<?> bookSave(@RequestBody BookSaveReqDto reqDto) {
-        return new ResponseEntity<>("잘 왔엉", HttpStatus.CREATED);
+        BookRespDto respDto = bookService.책등록하기(reqDto);
+        return new ResponseEntity<>(respDto, HttpStatus.CREATED);
+    }
+
+    // 대표적 - 정상(200), insert(201), 인증안됨(403), 클라이언트요청잘못(404), 서버쪽에러(500)
+    @GetMapping("/api/book/{id}")
+    public ResponseEntity<?> bookFindOne(@PathVariable Long id) {
+        BookRespDto respDto = bookService.책한건가져오기(id);
+        // return ResponseEntity.ok().body(respDto); 이렇게 쓸 수도 있지만 쓸 수 있는 상태코드가 적다
+        return new ResponseEntity<>(respDto, HttpStatus.OK);
     }
 }
